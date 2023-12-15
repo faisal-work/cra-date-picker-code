b9e0b141310613bd46ab485a646824983e8854a5
faisal-work <faisal@rently.com>
2023-12-16 02:13:34 +0530

    overiding cra webpack config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react'
-import DatePicker from 'react-date-picker';
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import DatePicker from "react-date-picker";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-      <div style={{width:'40%',height:'50%',display:'flex',flexDirection:'column',justifyContent:'center'}}>
-        <h1 style={{textAlign:'center'}}>DatePicker demo with cra</h1>
-        
-        <DatePicker />
-        
-        </div>
+      <div
+        style={{
+          width: "40%",
+          height: "50%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ textAlign: "center" }}>
+          DatePicker demo with create react app configuration overide
+        </h1>
 
+        <DatePicker />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
