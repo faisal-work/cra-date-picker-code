cca614ec35292789caf5d2d8292e5a46b901cc8a
faisal-work <faisal@rently.com>
2023-12-16 01:41:51 +0530

    validated working of date picker in CRA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import DatePicker from 'react-date-picker';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{width:'50%',height:'50%',display:'flex',flexDirection:'column',justifyContent:'center'}}>
+        <h1 style={{textAlign:'center'}}>DatePicker demo with cra</h1>
+        
+        <DatePicker />
+        
+        </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
